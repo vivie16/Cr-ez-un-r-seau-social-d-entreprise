33d057577190dca179bb3fea314bd8562552d444
vivie16 <virginiedevaine@gmail.com>
2022-07-29 17:25:31 +0200

    upload

---
+++ Front-end/src/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles/index.scss";
+import { Provider } from "react-redux";
+import { applyMiddleware, legacy_createStore } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+import { getUsers } from "./actions/users.actions";
+import { BrowserRouter} from 'react-router-dom';
+// dev tools
+import { composeWithDevTools } from "redux-devtools-extension";
+import { getPosts } from "./actions/post.actions";
+
+const store = legacy_createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+store.dispatch(getUsers());
+store.dispatch(getPosts());
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+);
